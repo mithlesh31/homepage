3cc82d1f3586d23ed74d559b016156d25e0d6888
mithlesh.gupta <mithlesh.gupta@navyuginfo.com>
2019-09-25 19:40:36 +0530

    inital setup

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Main.css';
+
+const Header = () => {
+
+    return (
+        <div className="home">
+            home
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Main.css';
+
+const Main = () => {
+
+    return (
+        <div className="home">
+            home
+
+        </div>
+    );
+};
+
+export default Main;
